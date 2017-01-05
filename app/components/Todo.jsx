1317a1b1646d05936a00998ad6da5793f1def48c
Dhrin Anantamongkolchai <dhrin_IMAC@Dhrins-iMac.local>
2017-01-06 02:07:58 +0700

    Add static todos

---
+++ app/components/Todo.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Todo = React.createClass ({
+	render: function () {
+
+		var {id, text} = this.props;
+
+		return (
+			<div>
+				<h4>{id}. {text}</h4>
+			</div>
+		)
+	}
+});
+
+module.exports = Todo;
