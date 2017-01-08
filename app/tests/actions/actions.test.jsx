a9c261c99594bf08354449a7256c53c6037767fb
Dhrin Anantamongkolchai <dhrin_IMAC@Dhrins-iMac.local>
2017-01-09 00:07:23 +0700

    Add redux actions

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,39 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+	it('should generante search text action', () => {
+		var action = {
+			type: 'SET_SEARCH_TEXT',
+			searchText: 'Some search text'
+		};
+		var res = actions.setSearchText(action.searchText);
+		expect(res).toEqual(action);
+	});
+
+	it('should generante add todo action', () => {
+		var action = {
+			type: 'ADD_TODO',
+			text: 'Sleep'
+		};
+		var res = actions.addTodo(action.text);
+		expect(res).toEqual(action);
+	});
+
+	it('should generante toggleShowCompleted', () => {
+		var action = {
+			type: 'TOGGLE_SHOW_COMPLETED'
+		};
+		var res = actions.toggleShowCompleted();
+		expect(res).toEqual(action);
+	});
+
+	it('should generante toogle todo', () => {
+		var action = {
+			type: 'TOGGLE_TODO',
+			id: 1
+		};
+		var res = actions.toggleTodo(action.id);
+		expect(res).toEqual(action);
+	});
+});
