421d2f233ed9db3e65c663db6b18b825c89ef7b9
Dhrin Anantamongkolchai <dhrin_IMAC@Dhrins-iMac.local>
2017-01-24 00:50:28 +0700

    config firebase with env variable

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
 	var config = {
-	    apiKey: "AIzaSyCmieq2RMDu_lhAyNH3CWZWCjVahp9s0ao",
-	    authDomain: "react-todo-app-3235c.firebaseapp.com",
-	    databaseURL: "https://react-todo-app-3235c.firebaseio.com",
-	    storageBucket: "react-todo-app-3235c.appspot.com",
-	    messagingSenderId: "872112226779"
+	    apiKey: process.env.API_KEY,
+	    authDomain: process.env.AUTH_DOMAIN,
+	    databaseURL: process.env.DATABASE_URL,
+	    storageBucket: process.env.STORAGE_BUCKET,
+	    messagingSenderId: process.env.MESSAGING_SENDER_ID
 	};
 
 	firebase.initializeApp(config);
